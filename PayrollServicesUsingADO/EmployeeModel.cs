18e0b7ee5630123e6f0c829409f84bba4bb57ea7
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-18 20:57:44 +0530

    [Dipti] added code to establish connection with created database

---
+++ PayrollServicesUsingADO/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollServicesUsingADO
+{
+    class EmployeeModel
+    {
+        //Getter and setter fields(present in db)
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public double BasicPay { get; set; }
+        public double Deductions { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double NetPay { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
---
+++ PayrollServicesUsingADO/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PayrollServicesUsingADO
+{
+    public class EmployeeRepository
+    {
+        //Connecting to Database
+        public static string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = payroleserviceADO; Integrated Security = True";
+        //passing the string to sqlconnection to make connection 
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
+    }
+}
