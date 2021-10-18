f508c88bba7986da638a5dcad19ffceedbe77bbf
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-18 21:07:59 +0530

    [Dipti] added code to retrieve all employee details from the database

--- PayrollServicesUsingADO/EmployeeRepository.cs
+++ PayrollServicesUsingADO/EmployeeRepository.cs
@@ -9,5 +9,55 @@ namespace PayrollServicesUsingADO
         public static string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = payroleserviceADO; Integrated Security = True";
         //passing the string to sqlconnection to make connection 
         SqlConnection sqlconnection = new SqlConnection(connectionString);
+
+        //GetAllEmployee method
+        public void GetAllEmployee()
+        {
+            try
+            {
+                //Creating object for employeemodel and access the fields
+                EmployeeModel employeeModel = new EmployeeModel();
+                //Retrieve query
+                string query = @"select * from employee_payroll";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
+                //Open the connection
+                this.sqlconnection.Open();
+                //ExecuteReader: Returns data as rows.
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        employeeModel.EmployeeId = Convert.ToInt32(reader["id"] == DBNull.Value ? default : reader["id"]);
+                        employeeModel.EmployeeName = reader["name"] == DBNull.Value ? default : reader["name"].ToString();
+                        employeeModel.BasicPay = Convert.ToDouble(reader["Base_Pay"] == DBNull.Value ? default : reader["Base_Pay"]);
+                        employeeModel.StartDate = (DateTime)(reader["startDate"] == DBNull.Value ? default(DateTime) : reader["startDate"]);
+                        employeeModel.Gender = reader["gender"] == DBNull.Value ? default : reader["gender"].ToString();
+                        employeeModel.Department = reader["department"] == DBNull.Value ? default : reader["department"].ToString();
+                        employeeModel.PhoneNumber = Convert.ToDouble(reader["phone"] == DBNull.Value ? default : reader["phone"]);
+                        employeeModel.Address = reader["address"] == DBNull.Value ? default : reader["address"].ToString();
+                        employeeModel.Deductions = Convert.ToDouble(reader["Deductions"] == DBNull.Value ? default : reader["Deductions"]);
+                        employeeModel.TaxablePay = Convert.ToDouble(reader["TaxablePay"] == DBNull.Value ? default : reader["TaxablePay"]);
+                        employeeModel.IncomeTax = Convert.ToDouble(reader["IncomeTax"] == DBNull.Value ? default : reader["IncomeTax"]);
+                        employeeModel.NetPay = Convert.ToDouble(reader["NetPay"] == DBNull.Value ? default : reader["NetPay"]);
+                        Console.WriteLine("{0} {1} {2}  {3} {4} {5}  {6}  {7} {8} {9} {10} {11}", employeeModel.EmployeeId, employeeModel.EmployeeName, employeeModel.BasicPay, employeeModel.StartDate, employeeModel.Gender, employeeModel.Department, employeeModel.PhoneNumber, employeeModel.Address, employeeModel.Deductions, employeeModel.TaxablePay, employeeModel.IncomeTax, employeeModel.NetPay);
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.sqlconnection.Close();
+            }
+        }
     }
 }
--- PayrollServicesUsingADO/Program.cs
+++ PayrollServicesUsingADO/Program.cs
@@ -10,6 +10,11 @@ namespace PayrollServicesUsingADO
     {
         static void Main(string[] args)
         {
+            //Creating a object for employeerepository
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
         }
     }
 }
